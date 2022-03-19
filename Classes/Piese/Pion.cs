48e245332ad06b66fa73c9862fb0e4bb1d0bbb3b
Alex Zamfir <72597190+m0b-x@users.noreply.github.com>
2022-03-19 11:32:49 +0200

    Safechecking-urile nu opresc total functia Functia ArataMutariPosibile are logica diferita

--- Classes/Piese/Pion.cs
+++ Classes/Piese/Pion.cs
@@ -31,61 +31,45 @@ namespace ProiectVolovici
             List<Pozitie> pozitii = new List<Pozitie>();
             if(CuloarePiesa == Culoare.Albastru)
             {
-                if(this.Pozitie.Linie == tabla.MarimeTablaOrizontala-1)
-                {
-                    return;
-                }
-                if (this.Pozitie.Linie <= tabla.PragRau )
+                int sfarsitLinie = tabla.MarimeTablaOrizontala - 1;
+                if (this.Pozitie.Linie != sfarsitLinie)
                 {
                     pozitii.Add(new Pozitie(this.Pozitie.Linie+1, this.Pozitie.Coloana ));
                 }
-                else if(this.Pozitie.Linie >= tabla.PragRau + 1)
+                if(this.Pozitie.Linie >= tabla.PragRau + 1)
                 {
                     int sfarsitColoana = tabla.MarimeTablaVerticala - 1;
-                    int sfarsitLinie = tabla.MarimeTablaOrizontala - 1;
-                    const int inceputColoana = 0;
-
-                    if (this.Pozitie.Coloana != sfarsitColoana)
-                    {
-                        pozitii.Add(new Pozitie(this.Pozitie.Linie, this.Pozitie.Coloana +1 ));
-                    }
-                    if (this.Pozitie.Linie != inceputColoana)
-                    {
-                        pozitii.Add(new Pozitie(this.Pozitie.Linie, this.Pozitie.Coloana - 1));
-                    }
-                    if (this.Pozitie.Linie != sfarsitLinie)
-                    {
-                        pozitii.Add(new Pozitie(this.Pozitie.Linie + 1, this.Pozitie.Coloana));
-                    }
-                }
-            }
-            else if (CuloarePiesa == Culoare.Alb)
-            {
-                if (this.Pozitie.Linie == 0)
-                {
-                    return;
-                }
-                if (this.Pozitie.Linie > tabla.PragRau)
-                {
-                    pozitii.Add(new Pozitie(this.Pozitie.Linie - 1, this.Pozitie.Coloana));
-                }
-                else if (this.Pozitie.Linie <= tabla.PragRau)
-                {
-                    int sfarsitColoana = tabla.MarimeTablaVerticala - 1;
-                    int inceputLinie = 0;
                     const int inceputColoana = 0;
 
                     if (this.Pozitie.Coloana != sfarsitColoana)
                     {
                         pozitii.Add(new Pozitie(this.Pozitie.Linie, this.Pozitie.Coloana + 1));
                     }
-                    if (this.Pozitie.Linie != inceputColoana)
+                    if (this.Pozitie.Coloana != inceputColoana)
                     {
                         pozitii.Add(new Pozitie(this.Pozitie.Linie, this.Pozitie.Coloana - 1));
                     }
-                    if (this.Pozitie.Linie != inceputLinie)
+                }
+            }
+            else if (CuloarePiesa == Culoare.Alb)
+            {
+                int inceputLinie = 0;
+                if (this.Pozitie.Linie != inceputLinie)
+                {
+                    pozitii.Add(new Pozitie(this.Pozitie.Linie - 1, this.Pozitie.Coloana));
+                }
+                if (this.Pozitie.Linie <= tabla.PragRau)
+                {
+                    int sfarsitColoana = tabla.MarimeTablaVerticala - 1;
+                    const int inceputColoana = 0;
+                    
+                    if (this.Pozitie.Coloana != sfarsitColoana)
                     {
-                        pozitii.Add(new Pozitie(this.Pozitie.Linie - 1, this.Pozitie.Coloana));
+                        pozitii.Add(new Pozitie(this.Pozitie.Linie, this.Pozitie.Coloana + 1));
+                    }
+                    if (this.Pozitie.Coloana != inceputColoana)
+                    {
+                        pozitii.Add(new Pozitie(this.Pozitie.Linie, this.Pozitie.Coloana - 1));
                     }
                 }
 
