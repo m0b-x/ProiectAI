6b9676933a44bac78078701b0ee0ad46768cbb80
Alex Zamfir <72597190+m0b-x@users.noreply.github.com>
2022-03-20 15:57:12 +0200

    Adaugat functie de codificare si decodificare mesaj

--- Classes/Tabla/ParserTabla.cs
+++ Classes/Tabla/ParserTabla.cs
@@ -22,7 +22,28 @@ namespace ProiectVolovici
         }
 
 
+        public String CodificareMutare(Pozitie pozitieInitiala,Pozitie pozitieFinala)
+        {
+            string mutareString =  "{" +
+                                    pozitieInitiala.Linie + "," +
+                                    pozitieInitiala.Coloana + "," +
+                                    pozitieFinala.Linie + "," +
+                                    pozitieFinala.Coloana +
+                                   "}";
+            return mutareString;
+        }
+        public Pozitie[] DecodificareMutare(String mutare)
+        {
+            mutare = mutare.Remove(0, 1);
+            mutare = mutare.Remove(mutare.Length);
+            int[] vectorPozitiiInt = mutare.Split(',').Select(int.Parse).ToArray();
+            Pozitie[] vectorPozitii = new Pozitie[2];
 
+            vectorPozitii[0] = new Pozitie(vectorPozitiiInt[0], vectorPozitiiInt[1]);
+            vectorPozitii[1] = new Pozitie(vectorPozitiiInt[2], vectorPozitiiInt[3]);
+
+            return vectorPozitii;
+        }
         public String CodificareTabla(int[,] matriceTabla)
         {
             var str = /*"mesajInceput" + */ string.Join("", matriceTabla.OfType<int>()
